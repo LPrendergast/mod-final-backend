6920a3badb294d1d4d25996233ffde8cf1f2f0ca
thetamicodes <reichold.tamara@gmail.com>
2019-11-13 14:03:49 +0000

    new and create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,4 +10,19 @@ class LocationsController < ApplicationController
     render json: @location
   end
 
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create(location_params)
+    render json: @location
+  end
+
+  private
+  
+  def location_params
+    params.require(:location).permit(:area, :description, :latitude, :longitude)
+  end
+
 end
